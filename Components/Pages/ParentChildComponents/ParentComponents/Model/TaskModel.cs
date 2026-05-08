eb70aa3a515f6787799d19df909cdc9fdd3a5a3e
kalasangili <kala9112005@gmail.com>
2026-05-08 18:37:00 +0530

    ADDED: Parent Child Communication

---
+++ Components/Pages/ParentChildComponents/ParentComponents/Model/TaskModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp1.Components.Pages.ParentChildComponents.ParentComponents.Model
+{
+    public class TaskModel
+    {
+        public int?  Id { get; set; }
+        public string? Task { get; set; }
+        public string? UserName { get; set; }
+    }
+}
