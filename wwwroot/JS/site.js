58ce1d78058d32aac5d528bf6fdd9cb52281eb74
kalasangili <kala9112005@gmail.com>
2026-05-25 18:38:11 +0530

    ADDED: Blazor to JS Communication

---
+++ wwwroot/JS/site.js
@@ -0,0 +1,11 @@
+﻿window.ConfirmMessage = function (message) {
+    return confirm(message);
+}
+
+window.AddTwoNumber = function (a,b) {
+    return a+b
+}
+
+function AlertMessageShown() {
+    alert("This is a Alert Message from site.js");
+}
