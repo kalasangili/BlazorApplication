5d1062c287563b19c6bfc03002444a1ce6d24636
kalasangili <kala9112005@gmail.com>
2026-05-26 19:13:32 +0530

    ADDED: Initial Shopping Cart Project

---
+++ Components/Pages/StateManagement/Model/CartModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp1.Components.Pages.StateManagement.Model
+{
+    public class CartModel
+    {
+        public string productName { get; set; }
+        public string Path { get; set; }
+        public float Price { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
