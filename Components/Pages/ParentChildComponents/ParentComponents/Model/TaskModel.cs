19cc4d49ff3eaacb35a37ce7d381d5d3955ac43e
kalasangili <kala9112005@gmail.com>
2026-05-26 11:15:33 +0530

    MODIFIED: Task Manager

--- Components/Pages/ParentChildComponents/ParentComponents/Model/TaskModel.cs
+++ Components/Pages/ParentChildComponents/ParentComponents/Model/TaskModel.cs
@@ -2,8 +2,10 @@
 {
     public class TaskModel
     {
-        public int?  Id { get; set; }
+        public Guid?  Id { get; set; }
         public string? Task { get; set; }
-        public string? UserName { get; set; }
+        public string? Priority { get; set; }
+        public string? Status { get; set; }
+
     }
 }
