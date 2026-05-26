8a4beea414ba6ed5e30ce98673924c1988f0cde7
kalasangili <kala9112005@gmail.com>
2026-05-26 12:54:38 +0530

    COMMIT: JS to Blazor Communication

--- wwwroot/JS/site.js
+++ wwwroot/JS/site.js
@@ -1,11 +1,36 @@
-﻿window.ConfirmMessage = function (message) {
+﻿
+let dotNetHelper;
+
+window.ConfirmMessage = function (message) {
     return confirm(message);
 }
 
 window.AddTwoNumber = function (a,b) {
-    return a+b
+    return a + b;
 }
 
 function AlertMessageShown() {
     alert("This is a Alert Message from site.js");
 }
+
+function AlertClickJSMethod() {
+    DotNet.invokeMethodAsync("BlazorApp1", "AlertMethod").then(result => {
+        alert(result);
+    });
+}
+
+window.AddJs = function (a,b) {
+    DotNet.invokeMethodAsync("BlazorApp1", "AddCsharp", a, b).then(result => {
+        console.log(result);
+    })
+}
+
+window.SetRef = function (helper) {
+    dotNetHelper = helper
+}
+
+window.InstanceMethodforAlert = function () {
+    dotNetHelper.invokeMethodAsync("AlertInstance").then(result => {
+        alert(result);
+    });
+} 
