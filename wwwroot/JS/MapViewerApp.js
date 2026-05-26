165e9e95c1899fabc45645a1bf8b572779e5b11a
kalasangili <kala9112005@gmail.com>
2026-05-26 19:04:04 +0530

    COMMIT: Map viewer app

--- wwwroot/JS/MapViewerApp.js
+++ wwwroot/JS/MapViewerApp.js
@@ -1,1 +1,17 @@
-﻿
+﻿window.loadMap = function () {
+
+    var map = L.map('map').setView([12.9716, 77.5946], 13);
+
+    L.tileLayer(
+        'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+        {
+            attribution: '&copy; OpenStreetMap contributors'
+        }
+    ).addTo(map);
+
+L.marker([12.9716, 77.5946])
+    .addTo(map)
+    .bindPopup("Bangalore")
+        .openPopup();
+
+}
