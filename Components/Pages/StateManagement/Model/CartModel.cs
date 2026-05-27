cd73eecbf07d55bf61168883f20cf6cd6548dc86
kalasangili <kala9112005@gmail.com>
2026-05-27 18:27:47 +0530

    MODIFIED: Shopping Cart Project using StateManagement

--- Components/Pages/StateManagement/Model/CartModel.cs
+++ Components/Pages/StateManagement/Model/CartModel.cs
@@ -5,7 +5,7 @@
         public string productName { get; set; }
         public string Path { get; set; }
         public float Price { get; set; }
-        public int Quantity { get; set; }
+        //public int Quantity { get; set; }
 
     }
 }
