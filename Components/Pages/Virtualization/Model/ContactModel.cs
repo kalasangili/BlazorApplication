b38ebb0e4f2e219636351b6fad1b02fbf4fef145
kalasangili <kala9112005@gmail.com>
2026-05-29 10:13:01 +0530

    ADDED: Handling Large Data with Virtualization

---
+++ Components/Pages/Virtualization/Model/ContactModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp1.Components.Pages.Virtualization.Model
+{
+    public class ContactModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Mail { get; set; }
+        //public string Phone { get; set; }
+    }
+}
